55a4b0d7b354c28a42539f2ef94a773e32661f85
angierobayo09 <angierobayo09@gmail.com>
2023-01-27 21:30:44 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let miNombre = new  String("Angie");
+let miApellido = new  String("Robayo");
+console.log(`Nombre: ${miNombre} ${miApellido}`);
+let miEdad = new Number(24);
+console.log(`Edad: ${miEdad}`);
+let esMujer = new Boolean(true);
+console.log(`Es mujer: ${esMujer}`);
+let otrosDatos = new Object({colorFavorito: "Azul", 
+                  animalPreferido: "Perro",
+                  frutaFavorita: "Granadilla"});
+console.log("Datos adicionales:");
+console.log({otrosDatos});
